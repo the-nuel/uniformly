53734cd4a36e89b03382ce69473b0b1aca547a8f
Tom Davies <tom@thenuel.com>
2019-02-09 19:22:37 +0000

    Adds support for React component libraries

--- uniformly.js
+++ uniformly.js
@@ -1,43 +1,59 @@
 #!/usr/bin/env node
-'use strict';
-const execute = require('./utils/execute');
-const yargs = require('yargs');
+"use strict";
+const execute = require("./utils/execute");
+const yargs = require("yargs");
 
-const config = require('./utils/configure')();
+const config = require("./utils/configure")();
 
 yargs
     .command(
-        ['build', 'transpile'],
-        'builds the project',
+        ["build", "transpile"],
+        "builds the project",
         yargs =>
             yargs
-                .option('output', {
-                    alias: ['out', 'o'],
-                    type: 'string',
-                    describe: 'the path where transpiled files should go',
-                    default: 'lib/'
+                .option("output", {
+                    alias: ["out", "o"],
+                    type: "string",
+                    describe: "the path where transpiled files should go",
+                    default: "lib/",
                 })
-                .option('source', {
-                    alias: ['src', 'in', 'i'],
-                    type: 'string',
-                    describe: 'the source directory to transpile',
-                    default: 'src/'
+                .option("source", {
+                    alias: ["src", "in", "i"],
+                    type: "string",
+                    describe: "the source directory to transpile",
+                    default: "src/",
                 }),
-        ({ source, out }) => {
-            execute('babel', [
+        ({ source, out, env }) => {
+            process.env.NODE_ENV = env || "development";
+            process.env.BABEL_ENV = env || "development";
+
+            execute("babel", [
                 source,
-                '-d',
+                "-d",
                 out,
-                '--config-file',
-                config.babel.config
+                "--config-file",
+                config.babel.config,
             ]);
         }
     )
-    .command('test', 'tests the project', () => {
-        execute('jest', ['--config', config.jest.config]);
+    .command(
+        "test",
+        "tests the project",
+        yargs => yargs,
+        ({ env }) => {
+            process.env.NODE_ENV = env || "test";
+            process.env.BABEL_ENV = env || "test";
+            execute("jest", ["--config", config.jest.config]);
+        }
+    )
+    .option("env", {
+        alias: ["environment", "node_env"],
+        type: "string",
+        describe: "overrides the value of NODE_ENV",
+        default: process.env.NODE_ENV,
     })
-    .alias('v', 'version')
-    .describe('version', 'show version')
+    .alias("v", "version")
+    .describe("version", "show version")
     .strict()
     .help()
-    .describe('help', 'show this help text').argv;
+    .describe("help", "show this help text").argv;
--- utils/configure.js
+++ utils/configure.js
@@ -1,21 +1,44 @@
-'use strict';
+"use strict";
+const fs = require("fs");
+const findup = require("find-up");
 let config = null;
 
-function resolveConfigFile(filename) {
-    return require.resolve(`../config/${filename}`);
+function resolveConfigFile(filename, folder) {
+    return require.resolve(
+        `../config/${folder ? `${folder}/` : ""}${filename}`
+    );
+}
+
+function findClosestPackage() {
+    const file = fs.readFileSync(findup.sync("package.json"), "utf-8");
+    return JSON.parse(file);
+}
+
+function isReactProject() {
+    const pkg = findClosestPackage();
+
+    return Object.keys({
+        ...pkg.dependencies,
+        ...pkg.peerDependencies,
+    }).includes("react");
 }
 
 module.exports = function() {
     if (!config) {
-        config = require('rc')('uniformly', {
+        config = require("rc")("uniformly", {
             babel: {
-                config: resolveConfigFile('babel.config.js')
+                config: resolveConfigFile(
+                    isReactProject() ? "react.config.js" : "default.config.js",
+                    "babel"
+                ),
             },
             jest: {
-                roots: ['src/'],
-                config: resolveConfigFile('jest.config.js'),
+                roots: ["src/"],
+                config: resolveConfigFile("jest.config.js"),
             },
-            node_modules: require('find-up').sync('node_modules', { cwd: __dirname })
+            node_modules: findup.sync("node_modules", {
+                cwd: __dirname,
+            }),
         });
     }
 
